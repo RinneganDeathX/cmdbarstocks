610d92477e914b22a7a958e5d50fff1eea7f4550
RinneganDeathX <80135618+RinneganDeathX@users.noreply.github.com>
2022-07-12 12:53:35 -0600

    Update .lua

--- .lua
+++ .lua
@@ -261,90 +261,7 @@ if TextBox.Text == "fixglitch" then
 
 	ballweld:Destroy()
 end
-if TextBox.Text == "sinisterpumpkin" then
-game.Players.LocalPlayer.Character.Hat.Mesh.MeshId="rbxassetid://36869418"game.Players.LocalPlayer.Character.Hat.Mesh.TextureId="rbxassetid://36869406"game.Players.LocalPlayer.Character.Hat.Mesh.Offset=Vector3.new(-0.0,0.0,0.0)game.Players.LocalPlayer.Character.Hat.Mesh.Scale=Vector3.new(1,1.1,1.1)
-end
--- Mascots
-if TextBox.Text == "grandpumpkin" then
-    game.Players.LocalPlayer.Character.Hat.Mesh.MeshId="rbxassetid://36869418"game.Players.LocalPlayer.Character.Hat.Mesh.TextureId="rbxassetid://36869406"game.Players.LocalPlayer.Character.Hat.Mesh.Offset=Vector3.new(-0.0,0.0,0.0)game.Players.LocalPlayer.Character.Hat.Mesh.Scale=Vector3.new(1,1.1,1.1)
-end
-if TextBox.Text == "sonic" then
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://166997534"
-	game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://37046037"
-	game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://430064078"
-	game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://430064079"
-	game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1135791438"
-	game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1135791544"
-	end
-if TextBox.Text == "mario" then
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1724867023"
-	game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://1725129162"
-	game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://494265290"
-	game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://494265293"
-	game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, 0.2, 0)
-	game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(0.6, 0.5, 0.7)
-	game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(0, -2, 0)
-	game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1, 1.04, 1)
-	game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1135791438"
-	game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1029104847"
-	game.Players.LocalPlayer.Character.Hair:Destroy()
-end
-if TextBox.Text == "hypebeast" then
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1301809529"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://1162338467"
-game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://734990739"
-game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://879096357"
-game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, 0.4, 0)
-game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(1.6, 1.12, 1.3)
-game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://433807755"
-game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://879096357"
-game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(0, -2, 0)
-game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1, 1.04, 1)
-game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1135791438"
-game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1029104847"
-game.Players.LocalPlayer.Character.Hair:Destroy()
-end
-if TextBox.Text == "packedman" then
-    game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://1221012541"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://433807755"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://1286599888"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(0, -1.9, 0)
-	game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1,1,1)
-	game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://69225450"
-	game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://1133198525"
-	game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(1, 1, 1)
-	game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, -0.2,-0.1)
-	game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1219235507"
-	game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1219235507"
-	game.Players.LocalPlayer.Character.Hair.Mesh.MeshId = "rbxassetid://734990739"
-	game.Players.LocalPlayer.Character.Hair.Mesh.Scale = Vector3.new(1.4, 1.1, 1.2)
-	game.Players.LocalPlayer.Character.Hair.Mesh.Offset = Vector3.new(0, 0.4, 0)
-end
-if TextBox.Text == "spondgebob" then 
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://110921637"
-	game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://3187407739"
-	game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://0"
-	game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://0"
-	game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, 0.4, 0)
-	game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(1.6, 1.12, 1.3)
-	game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://0"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://0"
-	game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(0, -2, 0)
-	game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1, 1.04, 1)
-	game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1029104847"
-	game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1029104847"
-	game.Players.LocalPlayer.Character.Head.Transparency = 1 
-	game.Players.LocalPlayer.Character.Head.face.Transparency = 1 
-	game.Players.LocalPlayer.Character.Hair:Destroy()
-	game:service('Players').LocalPlayer.Character["Body Colors"].TorsoColor = BrickColor.new('New Yeller')
-	game:service('Players').LocalPlayer.Character["Body Colors"].RightArmColor = BrickColor.new('New Yeller')
-	game:service('Players').LocalPlayer.Character["Body Colors"].HeadColor = BrickColor.new('New Yeller')
-	game:service('Players').LocalPlayer.Character["Body Colors"].LeftLegColor = BrickColor.new('New Yeller')
-	game:service('Players').LocalPlayer.Character["Body Colors"].RightLegColor = BrickColor.new('New Yeller')
-	game:service('Players').LocalPlayer.Character["Body Colors"].LeftArmColor = BrickColor.new('New Yeller')
-end
+
 if TextBox.Text == "tp" then
     local plr = game.Players.LocalPlayer
  
@@ -1000,93 +917,7 @@ plr.UserId = 4925605
 play.Visible = false
 new.Visible = true
 end
-if TextBox.Text == "obito" then 
-      game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://406930360"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://406927829"
-game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://1415156037"
-game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://2675785344"
-game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, 0.39, 0.12)
-game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(2, 2.1, 2.2)
-game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://597760587"
-game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://597760909"
-game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(-0, 0, -0.6)
-game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1.9, 1.5, 1.3)
-game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1028523033"
-game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1029104847"
-game.Players.LocalPlayer.Character.Hair:Destroy()
-end
-if TextBox.Text == "bluefit" then
-     game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1160827659"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://1221012541"
-game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://433807755"
-game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://1286599888"
-game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(0, -1.9, 0)
-game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1,1,1)
-game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://69225450"
-game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://1133198525"
-game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(1, 1, 1)
-game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, -0.2,-0.1)
-game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://1219235507"
-game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://1219235507"
-game.Players.LocalPlayer.Character.Hair.Mesh.MeshId = "rbxassetid://734990739"
-game.Players.LocalPlayer.Character.Hair.Mesh.Scale = Vector3.new(1.4, 1.1, 1.2)
-game.Players.LocalPlayer.Character.Hair.Mesh.Offset = Vector3.new(0, 0.4, 0)
-end
-if TextBox.text == "cailou" then
- game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://3135982037"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://3101522275"
-game.Players.LocalPlayer.Character.Hat.Mesh.MeshId = "rbxassetid://0"
-game.Players.LocalPlayer.Character.Hat.Mesh.TextureId = "rbxassetid://0"
-game.Players.LocalPlayer.Character.Hat.Mesh.Offset = Vector3.new(0, 0.4, 0)
-game.Players.LocalPlayer.Character.Hat.Mesh.Scale = Vector3.new(1.6, 1.12, 1.3)
-game.Players.LocalPlayer.Character.Hat2.Mesh.MeshId = "rbxassetid://433807755"
-game.Players.LocalPlayer.Character.Hat2.Mesh.TextureId = "rbxassetid://879096357"
-game.Players.LocalPlayer.Character.Hat2.Mesh.Offset = Vector3.new(0, -2, 0)
-game.Players.LocalPlayer.Character.Hat2.Mesh.Scale = Vector3.new(1, 1.04, 1)
-game.Players.LocalPlayer.Character.ShoeL.TextureID = "rbxassetid://100370086"
-game.Players.LocalPlayer.Character.ShoeR.TextureID = "rbxassetid://100370086"
-game.Players.LocalPlayer.Character.Hair:Destroy()
-end
-if TextBox.Text == "autoscore" then 
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/RinneganDeathX/rimautoscore/main/.lua"), true))()
-end
-if TextBox.Text == "pumpkinoutfit" then 
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1078590154"
-    game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://1078590154"
-    game.Players.LocalPlayer.Character.Hat.Mesh.MeshId="rbxassetid://36869418"game.Players.LocalPlayer.Character.Hat.Mesh.TextureId="rbxassetid://36869406"game.Players.LocalPlayer.Character.Hat.Mesh.Offset=Vector3.new(-0.0,0.0,0.0)game.Players.LocalPlayer.Character.Hat.Mesh.Scale=Vector3.new(1,1.1,1.1)
-end     
-if TextBox.Text == "jersey1" then 
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://8082566504"
-end
-if TextBox.Text == "jersey2" then 
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://2011007775"
-end
-if TextBox.Text == "jersey3" then 
-     game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://5173220107"
-end
-if TextBox.Text == "jersey4" then 
-     game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://7153516802"
-end
-if TextBox.Text == "jersey5" then 
-     game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://8655892886"
-end
-if TextBox.Text == "jersey6" then 
-     game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://6870874749"
-end
-if TextBox.Text == "jersey7" then 
-    game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://9665608448"
-end
-if TextBox.Text == "jersey8" then 
-      game.Players.LocalPlayer.Character.Shirt.ShirtTemplate = "rbxassetid://1275282054"
-game.Players.LocalPlayer.Character.Pants.PantsTemplate = "rbxassetid://6281083949"
-end
+
 if TextBox.Text == "trashstat" then 
     for i,v in pairs(getgc(true)) do
     if type(v) == "table" and rawget(v, "handles") then
@@ -1208,6 +1039,9 @@ local Event = game:GetService("ReplicatedStorage")["join.court"]
 Event:InvokeServer(A_1, A_2)
 wait(2)
 end
+end
+if TextBox.Text == "clothing" then
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/RinneganDeathX/clothingrbhub/main/.lua"), true))()
 end
 end)
 
